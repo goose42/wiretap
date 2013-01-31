a211f704b1367968a50a9ee58b8e7190f3df4210
Jayanth <vjayanth@indiana.edu>
2013-01-30 22:10:38 -0500

    some changed to make the code look nicer

--- read_fns.cpp
+++ read_fns.cpp
@@ -50,7 +50,7 @@ void cap_file::pcap_read (FILE *fp)
 
 	//read the file for packets here
 	pcap_loop(handle, -1, cap_file::got_packet, user_data); 	
-
+	pcap_close(handle);
 	aggr_packet.output_content();
   	
 	return;
--- wiretap.cpp
+++ wiretap.cpp
@@ -1,36 +1,35 @@
-#include <iostream>
-
 
 #include "read_fns.h"
-using std::cout;
-using std::endl;
 
-
+#include <iostream>
 
 int main(int argc,char* argv[])
 {
-	FILE *input_file;
+    using std::cout;
+    using std::endl;
+    FILE *input_file;
 
-	if (argc != 2) //check if correct number of arguments
-	{
-		cout<<"Incorrect method to execute wiretap."<<endl<<"Please run wiretap with the following command:"<<endl<<"\t./wiretap <pcap dump file name>"<<endl;
-		return 0;
-	}
-	input_file = fopen(argv[1], "r"); //open file
+    //check if correct number of arguments 
+    if (argc != 2) {
+        std::cout << "Incorrect method to execute"
+            " wiretap." << endl << "Please run wiretap with the following"
+            " command:" << endl << "\t./wiretap <pcap dump file name>" <<
+            endl;
+        return 0;
+    }
 
-	if (input_file == NULL) //check if file opens correctly.
-	{
-		cout<<"Error with opening file, please check the file name"<<endl;
-		return 0;
-	}  
+    input_file = fopen(argv[1], "r"); //open file
 
-	//once file is opened, pass it into the pcap method to read and make sense of
-	cap_file::pcap_read(input_file); 
+    //check if file opens correctly.
+    if (input_file == NULL) {
+        cout << "Error with opening file, please check the file name" << endl;
+        return 0;
+    }  
 
-	return 0;
+    //once file is opened, pass it into the pcap method to read and make sense of
+    cap_file::pcap_read(input_file); 
+
+    fclose(input_file);
+    return 0;
 }
-
-
-
-
 
